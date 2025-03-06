3d3e2c0f8e3536582b17edf93f454a035e39b4cc
adigeorg <adigeorg@iu.edu>
2025-03-05 21:15:40 -0500

    changes made to main.py

--- exercise_2/main.py
+++ exercise_2/main.py
@@ -9,13 +9,12 @@ https://www.hddaccess.com/tips/searching-in-snomed-ct-using-ecl-2
 
 
 def constraint_1():
-    # Search all clinical findings that has an associated morphology of caries and finding site that is a tooth part
     constraint = """
-< 404684003 |Clinical finding (finding)| :
+<< 404684003 |Clinical finding (finding)| :
 
-<< 116676008 |Associated morphology (attribute)| = << 65413006 |Caries (morphologic abnormality)|
+<< 116676008 |Associated morphology (attribute)| = << 44132006 |Abscess (morphologic abnormality)|
 
-AND << 363698007 |Finding site (attribute)| = << 410613002 |Tooth part (body structure)|
+AND << 363698007 |Finding site (attribute)| = << 113279002 |Gingival structure (body structure)|
     """
     return constraint
 
@@ -24,10 +23,10 @@ def expression_constraint(search_string):
     response = requests.get(f'{BASE_HERMES_URL}/search?constraint={search_string}')
     data = response.json()
     for item in data:
-        print(item)
+        print(f"{item['term']} | {item['preferredTerm']} | {item['conceptId']}" )
 
 
-if __name__ == '__main__':
+if __name__ == '_main_':
     string_value = constraint_1().strip()
     print()
     expression_constraint(search_string=string_value)
